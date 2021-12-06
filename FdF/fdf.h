f6aeb894296858a87d2a94eb268ea78451af984d
cdiaz13 <85074433+cdiaz13@users.noreply.github.com>
2021-12-06 12:45:19 +0100

    Add files via upload

--- FdF/fdf.h
+++ FdF/fdf.h
@@ -18,7 +18,16 @@ typedef struct	s_map {
 	int		**f_map;
 	int	i;
 	int	j;
+	int	dx;
+	int	dy;
+	int	p;
 }	t_map;
+
+typedef struct	s_dot {
+	int	x;
+	int	y;
+	int	z;
+} t_dot;
 
 int	main(int argc, char **argv);
 void	ft_get_map_size(t_map *map, char *argv, int rows, int cols);
@@ -26,10 +35,11 @@ void	ft_create_map(t_map *map, char *argv);
 void	ft_init_mlx(t_map *map);
 void	ft_create_grid(t_map *map, int x, int y, t_data *img);
 void	my_mlx_pixel_put(t_data *data, int width, int height, int color);
-void	ft_center_map(t_map *map, int *x, int *y);
-void	ft_iso(int x, int y, t_data *img, int z);
-void	ft_x_dots(t_map *map, int x1, int y1, int z);
-void	ft_y_dots(t_map *map, int x1, int y1, int z);
+void	ft_x_dots(t_map *map, t_data *img, int x, int y);
+void	ft_y_dots(t_map *map, t_data *img, int x, int y);
+void	ft_bresenham_high(t_map *map, t_data *img, t_dot *a, t_dot *b);
+void	ft_bresenham_equal(t_data *img, t_dot *a, t_dot *b);
+void	ft_bresenham_low(t_map *map, t_data *img, t_dot *a, t_dot *b);
 
 
 
